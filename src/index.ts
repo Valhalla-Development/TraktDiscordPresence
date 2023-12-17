8b4db6d9f78a7c145f07b2102cdf8a178143b0b4
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-17 16:06:54 +0000

    ✨ feat(index.ts): this is now functional... read description!
    My first goal for rewriting this in Typescript, was to just convert my existing code to Typescript... This is now done, now I plan to rewrite a large portion of it, to improve maintainability, readability, and efficiency

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'colors';
-import { existsSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import Enquirer from 'enquirer';
 // @ts-ignore
 import Trakt from 'trakt.tv';
+import { Client } from 'discord-rpc';
+import { DateTime } from 'luxon';
 
 const { prompt } = Enquirer;
 
@@ -13,7 +15,106 @@ interface TraktCredentials {
     oAuth?: string;
 }
 
-// Extract repetitive structure into a separate function
+interface TraktContent {
+    smallImageKey: string;
+    largeImageKey: string;
+    startTimestamp: Date;
+    details?: string;
+    state?: string
+}
+
+async function fetchTraktCredentials(): Promise<TraktCredentials> {
+    const configData = readFileSync('./config.json', 'utf8');
+    return JSON.parse(configData);
+}
+
+let rpc: Client | null;
+// eslint-disable-next-line no-undef
+let statusInt: string | number | NodeJS.Timeout | undefined;
+
+const spawnRPC = async (trakt: Trakt) => {
+    try {
+        console.log(trakt);
+        const traktCredentials = await fetchTraktCredentials();
+        rpc = new Client({ transport: 'ipc' });
+
+        rpc.on('error', (err) => {
+            console.log(err);
+        });
+        rpc.on('ready', () => {
+            console.log('Successfully connected to Discord!'.green);
+        });
+        await rpc.login({ clientId: traktCredentials.discordClientId });
+        await updateStatus(trakt);
+
+        statusInt = setInterval(() => {
+            updateStatus(trakt);
+        }, 15000);
+    } catch (err) {
+        console.log('Failed to connect to Discord. Retrying in 15 seconds.'.red);
+        setTimeout(() => {
+            spawnRPC(trakt);
+        }, 15000);
+    }
+};
+
+async function createTrakt() {
+    const traktCredentials = await fetchTraktCredentials();
+    const trakt = new Trakt({
+        client_id: traktCredentials.clientId,
+        client_secret: traktCredentials.clientSecret,
+    });
+    trakt.import_token(traktCredentials.oAuth);
+    return trakt;
+}
+
+async function updateStatus(trakt: Trakt) {
+    if (rpc) {
+        // @ts-ignore
+        if (rpc.transport.socket.readyState !== 'open') {
+            clearInterval(statusInt);
+            await rpc.destroy();
+            rpc = null;
+            await spawnRPC(trakt);
+            return;
+        }
+
+        const user = await trakt.users.settings();
+        const watching = await trakt.users.watching({ username: user.user.username });
+
+        if (watching) {
+            const type: TraktContent = {
+                smallImageKey: 'play',
+                largeImageKey: 'trakt',
+                startTimestamp: new Date(watching.started_at),
+            };
+
+            // Set the activity
+            if (watching.type === 'movie') {
+                const { movie } = watching;
+                type.details = `${movie.title} (${movie.year})`;
+            } else if (watching.type === 'episode') {
+                const { show, episode } = watching;
+                type.details = `${show.title}`;
+                type.state = `S${episode.season}E${episode.number} (${episode.title})`;
+            }
+            await rpc.setActivity({ ...type });
+
+            console.log(`${formatDate()} | ${'Trakt Playing:'.red} ${type.details}${type.state ? ` - ${type.state}` : ''}`);
+        } else {
+            // Check if the user is currently watching something and if not, run on a timeout.
+            console.log(`${formatDate()} | ${'Trakt:'.red.underline} Not Playing.`);
+            await rpc.clearActivity();
+        }
+    }
+}
+
+function formatDate() {
+    const now = DateTime.now();
+
+    return `${now.toLocaleString(DateTime.DATE_HUGE)} - ${now.toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET)}`.green.italic;
+}
+
 function generatePromptConfig(name: string, message: string) {
     return {
         type: 'input',
@@ -29,7 +130,6 @@ async function generateTraktCredentials(): Promise<TraktCredentials | null> {
         }`,
     );
 
-    // Utilize the newly defined function to create prompt config
     return prompt([
         generatePromptConfig('clientId', 'Please provide your Trakt Client ID:'),
         generatePromptConfig('clientSecret', 'Please provide your Trakt Client Secret:'),
@@ -73,16 +173,16 @@ async function authoriseTrakt(gen: TraktCredentials) {
 
 async function main() {
     try {
-        // Execute the function to generate configuration if it doesn't already exist
         if (!existsSync('./config.json')) {
             const gen = await generateTraktCredentials();
             if (gen) await authoriseTrakt(gen);
         }
+        const trakt = await createTrakt();
+        await spawnRPC(trakt);
     } catch (error) {
         console.error(`\nAn error occurred: ${error}`.red);
         process.exit(1);
     }
 }
 
-// Execute the main function
 main();
