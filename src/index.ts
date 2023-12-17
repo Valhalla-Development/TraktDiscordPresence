1ddac097c95b9fd3e92294fbe187d00634b8484f
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-12-17 16:34:14 +0000

    ✨ feat(index.ts): refactor code to use classes for DiscordRPC and TraktInstance to improve code organization and reusability
    This is rough, some comments blocking errors, the reason I am including this, is so you can see my progress :)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import 'colors';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
+import 'colors';
 import Enquirer from 'enquirer';
-// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import Trakt from 'trakt.tv';
 import { Client } from 'discord-rpc';
 import { DateTime } from 'luxon';
@@ -23,90 +24,95 @@ interface TraktContent {
     state?: string
 }
 
+class DiscordRPC {
+    rpc: Client | null;
+
+    statusInt: NodeJS.Timeout | undefined;
+
+    constructor() {
+        this.rpc = null;
+    }
+
+    async spawnRPC(trakt: TraktInstance) {
+        try {
+            const traktCredentials = await fetchTraktCredentials();
+            this.rpc = new Client({ transport: 'ipc' });
+            this.rpc.on('ready', () => {
+                console.log('Successfully connected to Discord!'.green);
+            });
+            await this.rpc.login({ clientId: traktCredentials.discordClientId });
+            await trakt.updateStatus(this.rpc, this.statusInt);
+            this.statusInt = setInterval(async () => {
+                await trakt.updateStatus(this.rpc, this.statusInt);
+            }, 15000);
+        } catch {
+            console.log('Failed to connect to Discord. Retrying in 15 seconds.'.red);
+            setTimeout(() => {
+                this.spawnRPC(trakt);
+            }, 15000);
+        }
+    }
+}
+
+class TraktInstance {
+    trakt: Trakt;
+
+    async createTrakt() {
+        const traktCredentials = await fetchTraktCredentials();
+        this.trakt = new Trakt({
+            client_id: traktCredentials.clientId,
+            client_secret: traktCredentials.clientSecret,
+        });
+        this.trakt.import_token(traktCredentials.oAuth);
+        return this.trakt;
+    }
+
+    // eslint-disable-next-line no-undef
+    async updateStatus(rpc: Client | null, statusInt: string | number | NodeJS.Timeout | undefined) {
+        if (rpc) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            if (rpc.transport.socket.readyState !== 'open') {
+                if (statusInt) clearInterval(statusInt);
+                await rpc.destroy();
+                // eslint-disable-next-line no-param-reassign
+                rpc = null;
+                await new DiscordRPC().spawnRPC(this);
+                return;
+            }
+
+            const user = await this.trakt.users.settings();
+            const watching = await this.trakt.users.watching({ username: user.user.username });
+
+            if (watching) {
+                const type: TraktContent = {
+                    smallImageKey: 'play',
+                    largeImageKey: 'trakt',
+                    startTimestamp: new Date(watching.started_at),
+                };
+
+                if (watching.type === 'movie') {
+                    const { movie } = watching;
+                    type.details = `${movie.title} (${movie.year})`;
+                } else if (watching.type === 'episode') {
+                    const { show, episode } = watching;
+                    type.details = `${show.title}`;
+                    type.state = `S${episode.season}E${episode.number} (${episode.title})`;
+                }
+                await rpc.setActivity({ ...type });
+
+                console.log(`${formatDate()} | ${'Trakt Playing:'.red} ${type.details}${type.state ? ` - ${type.state}` : ''}`);
+            } else {
+                console.log(`${formatDate()} | ${'Trakt:'.red.underline} Not Playing.`);
+                await rpc.clearActivity();
+            }
+        }
+    }
+}
+
 async function fetchTraktCredentials(): Promise<TraktCredentials> {
     const configData = readFileSync('./config.json', 'utf8');
     return JSON.parse(configData);
-}
-
-let rpc: Client | null;
-// eslint-disable-next-line no-undef
-let statusInt: string | number | NodeJS.Timeout | undefined;
-
-const spawnRPC = async (trakt: Trakt) => {
-    try {
-        console.log(trakt);
-        const traktCredentials = await fetchTraktCredentials();
-        rpc = new Client({ transport: 'ipc' });
-
-        rpc.on('error', (err) => {
-            console.log(err);
-        });
-        rpc.on('ready', () => {
-            console.log('Successfully connected to Discord!'.green);
-        });
-        await rpc.login({ clientId: traktCredentials.discordClientId });
-        await updateStatus(trakt);
-
-        statusInt = setInterval(() => {
-            updateStatus(trakt);
-        }, 15000);
-    } catch (err) {
-        console.log('Failed to connect to Discord. Retrying in 15 seconds.'.red);
-        setTimeout(() => {
-            spawnRPC(trakt);
-        }, 15000);
-    }
-};
-
-async function createTrakt() {
-    const traktCredentials = await fetchTraktCredentials();
-    const trakt = new Trakt({
-        client_id: traktCredentials.clientId,
-        client_secret: traktCredentials.clientSecret,
-    });
-    trakt.import_token(traktCredentials.oAuth);
-    return trakt;
-}
-
-async function updateStatus(trakt: Trakt) {
-    if (rpc) {
-        // @ts-ignore
-        if (rpc.transport.socket.readyState !== 'open') {
-            clearInterval(statusInt);
-            await rpc.destroy();
-            rpc = null;
-            await spawnRPC(trakt);
-            return;
-        }
-
-        const user = await trakt.users.settings();
-        const watching = await trakt.users.watching({ username: user.user.username });
-
-        if (watching) {
-            const type: TraktContent = {
-                smallImageKey: 'play',
-                largeImageKey: 'trakt',
-                startTimestamp: new Date(watching.started_at),
-            };
-
-            // Set the activity
-            if (watching.type === 'movie') {
-                const { movie } = watching;
-                type.details = `${movie.title} (${movie.year})`;
-            } else if (watching.type === 'episode') {
-                const { show, episode } = watching;
-                type.details = `${show.title}`;
-                type.state = `S${episode.season}E${episode.number} (${episode.title})`;
-            }
-            await rpc.setActivity({ ...type });
-
-            console.log(`${formatDate()} | ${'Trakt Playing:'.red} ${type.details}${type.state ? ` - ${type.state}` : ''}`);
-        } else {
-            // Check if the user is currently watching something and if not, run on a timeout.
-            console.log(`${formatDate()} | ${'Trakt:'.red.underline} Not Playing.`);
-            await rpc.clearActivity();
-        }
-    }
 }
 
 function formatDate() {
@@ -171,18 +177,23 @@ async function authoriseTrakt(gen: TraktCredentials) {
     process.exit(1);
 }
 
+async function initializeTraktAndDiscordRPC() {
+    const traktInstance = new TraktInstance();
+    await traktInstance.createTrakt();
+    const discordRPC = new DiscordRPC();
+    await discordRPC.spawnRPC(traktInstance);
+}
+
 async function main() {
     try {
         if (!existsSync('./config.json')) {
-            const gen = await generateTraktCredentials();
-            if (gen) await authoriseTrakt(gen);
+            const generatedCredentials = await generateTraktCredentials();
+            if (generatedCredentials) await authoriseTrakt(generatedCredentials);
         }
-        const trakt = await createTrakt();
-        await spawnRPC(trakt);
+        await initializeTraktAndDiscordRPC();
     } catch (error) {
         console.error(`\nAn error occurred: ${error}`.red);
         process.exit(1);
     }
 }
-
-main();
+await main();
